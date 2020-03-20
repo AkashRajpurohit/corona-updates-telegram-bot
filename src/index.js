d7a64f17af1ecbb63afd7f6b1e8809f3ceb378d1
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2020-03-20 19:55:19 +0530

    :art: now testing for telegram bot

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 require('dotenv').config();
-const getInformation = require('./utils/getInformation');
+// const getInformation = require('./utils/getInformation');
+const TelegramBot = require('node-telegram-bot-api');
+const TOKEN = process.env.TELEGRAM_TOKEN;
+const options = {
+  webHook: {
+    port: 443
+  }
+};
 
-(async() => {
-    const result = await getInformation()
-    console.log(result)
-})()
+const url = process.env.NOW_URL;
+const bot = new TelegramBot(TOKEN, options);
+
+
+// This informs the Telegram servers of the new webhook.
+// Note: we do not need to pass in the cert, as it already provided
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+
+// Just to ping!
+bot.on('message', function onMessage(msg) {
+  bot.sendMessage(msg.chat.id, 'I am alive on Zeit Now!');
+});
