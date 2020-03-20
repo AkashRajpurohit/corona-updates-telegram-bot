2351b9d2ac428014e19106af83f9b017ac91464f
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2020-03-20 19:24:12 +0530

    :art: get new documents uploaded links

--- src/index.js
+++ src/index.js
@@ -3,31 +3,61 @@ const cheerio = require('cheerio')
 
 const getRawBody = require('./utils/getRawBody')
 
-const data = []
-
 const getData = async () => {
+    const stateData = []
+    const newDocumentPdfLinks = []
     try {
         const html = await getRawBody(process.env.DATA_BASE_URL)
         const $ = cheerio.load(html)
 
+        const tableHeadRow = $('div.table-responsive table thead tr')
+        const tableHeader = []
+        tableHeadRow.children().each((index, row) => {
+            if(index !== 0) {
+                tableHeader.push($(row).text().trim())
+            } 
+        })
+
+        stateData.push(tableHeader)
+
         const tableBody = $('div.table-responsive table tbody')
         
-        tableBody.children().each((index, element) => {
+        tableBody.children().each((_, element) => {
             const perStateData = []
             const rows = $(element).find('td')
 
             if(rows.length === 5) {
                 perStateData.push("Total number of confirmed cases in India")
             }
-            rows.each((i, elem) => {
+            rows.each((i, row) => {
                 if(i !== 0) {
-                    perStateData.push($(elem).text().trim())
+                    perStateData.push($(row).text().trim())
                 } 
             })
-            data.push(perStateData)
+
+            stateData.push(perStateData)
         })
-        
-        console.log(JSON.stringify(data, null, 2))
+
+        const newDocuments = $('img[alt="New"]')
+
+        newDocuments.each((i, doc) => {
+            let pdfLink = $(doc).parent().parent().parent().attr('href')
+            let pdfTitle = $(doc).parent().parent().parent().text().trim()
+            if(pdfLink === undefined) {
+                pdfLink = $(doc).parent().attr('href')
+                pdfTitle = $(doc).parent().text().trim()
+            }
+
+            if(pdfLink !== undefined) {
+                newDocumentPdfLinks.push({
+                    link: `${process.env.DATA_BASE_URL}${pdfLink}`,
+                    title: pdfTitle
+                })
+            }
+        })
+
+        console.log(stateData, newDocumentPdfLinks)
+
     } catch(e) {
         if(e.statusCode === 500) {
             console.log("Cannot load data")
