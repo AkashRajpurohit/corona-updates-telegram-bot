e41fb11813b8a31c747ce1457b9bebb7cf3dfa43
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2020-03-21 12:24:44 +0530

    :art: show keyboard when option /info choosed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 require('dotenv').config();
-// const getInformation = require('./utils/getInformation');
 const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+
+const getInformation = require('./utils/getInformation');
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
 
-bot.start((ctx) => ctx.reply('Welcome'))
+bot.start((ctx) => ctx.reply('Welcome! Please use the /help command to check the features provided by me'))
+
+bot.help((ctx) => ctx.reply('List features here in depth'))
+
+bot.command('/info', async (ctx) => {
+    return ctx.reply('What do you want to view', Markup
+    .keyboard([
+      ['📊 Current Situation in India'],
+      ['📰 New Articles Shared by Government']
+    ])
+    .oneTime()
+    .resize()
+    .extra()
+  )
+})
+
+bot.hears('📊 Current Situation in India', (ctx) => {
+    ctx.reply('return stats')
+})
+
+bot.hears('📰 New Articles Shared by Government', (ctx) => {
+    ctx.reply('return list of articles')
+})
 
 bot.launch()
     .then(() => console.log('Bot started'))
