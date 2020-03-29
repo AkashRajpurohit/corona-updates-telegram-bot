27d7b90b7a1251ae591496f009238d79706dcee2
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2020-03-29 16:22:00 +0530

    :bug: extra empty array removed

--- src/utils/replyWithStateInformationInHTML.js
+++ src/utils/replyWithStateInformationInHTML.js
@@ -4,7 +4,7 @@ module.exports = async (data, reply) => {
     let output = ''
     const title = ['State', 'Confirmed (Indian National)', 'Confirmed (Foreign National)', 'Cured', 'Death']
 
-    for(let i = 0; i < data.length - 2; i++) {
+    for(let i = 0; i < data.length - 1; i++) {
         for(let j = 0; j < data[i].length; j++) {
             output += `<i>${title[j]}:</i> <b>${data[i][j]}</b> \n\n`
         }
@@ -16,8 +16,8 @@ module.exports = async (data, reply) => {
 
     output += '<b>Total All Over India</b> \n\n'
 
-    for(let i = 1; i < data[data.length - 2].length; i++) {
-        output += `<i>${title[i]}:</i> <b>${data[data.length - 2][i]}</b> \n\n`
+    for(let i = 1; i < data[data.length - 1].length; i++) {
+        output += `<i>${title[i]}:</i> <b>${data[data.length - 1][i]}</b> \n\n`
     }
 
     await reply(output)
